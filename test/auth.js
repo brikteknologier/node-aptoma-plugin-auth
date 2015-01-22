22874117138f0ff194f22e15d6c80aab76cacc2f
Helge Skogly Holm <helge.holm@gmail.com>
2015-01-22 14:40:07 +0100

    Specified in test that signature is understood by both an OpenSSL and an MCrypt implementation.

--- test/auth.js
+++ test/auth.js
@@ -8,6 +8,12 @@ describe("node-aptoma-auth", function() {
   var APPKEY = 'DrPublish';
   var AUTH = "VwrfUNpTSILU4pB0gindSIc1kcZfqqpppY3Z67h4Vv YeWmhSXXRYnjUXWcJGW/DKvDsPoDUzpNeCnFxXsmn3UanSiYE06Wfz0EYP6pIIZFh7E8lRJyy7Fc3Du5I22sx2GmjkQej5JK0NyMk4IQiUGr17tyandwodaDkPtsGSijnj9X824YuGHhX4AsDOTCEVP26bz18bEXwTymPanbpkq2AIQ0oZ7msZ0TM44QP7wmtk9ZDsTHWtEyY2aGP6mCzXtqNwPDo9EGoiHAex i6ea/gNsRfd63kLZAKhfKVHvav3ysf1iB82x5NdcGXXbaPKVPiEnNm0 tHcg2H Zq3BVKNwRWwJ96DSs4BpDQDZT2ExZ6r/DZ0jFzzvmmOhhxClqqwDyp0BsSl/5DFofIlz2pXuc7qkSqRICs2q8SY14bq7cl2ze2GxuP4cqm5v5io5TVrVm9zXeH34SRBOv3s/Sp6YaNqG lNBXGT0X3QDg3gy SNqo5ZeEttoheZNI3Wo5Rklxxd7zkoeSEnK3vtKM64dZovk3ZlffZaDxymsVP5niggg8a0E49Zh/MjU28aLiyg9t7N7iG W4vv92BbVEPRre5AkfJxGcQyxQum2BhNgpBFRACFMt4lOGdvquT9";
   var IV = "bddf86c1af538c02";
+  var keyBuf = (function() {
+    var keyBase = APPKEY + 'http://drpubapp.brik.no:80';
+    var keySha1 = crypto.createHash('sha1').update(keyBase).digest('hex');
+    var keyHex = keySha1.slice(0, 32);
+    return new Buffer(keyHex);
+  })();
 
   beforeEach(function() {
     app = appInit(
@@ -68,20 +74,31 @@ describe("node-aptoma-auth", function() {
     assert(token.iv);
   });
 
-  it("returns a valid token object", function() {
+  it("returns a valid token object (MCrypt)", function() {
     var token = app.getAuthenticationToken();
     var td = new mcrypt.MCrypt('rijndael-128', 'cbc');
-    var key = (function() {
-      var keyBase = APPKEY + 'http://drpubapp.brik.no:80';
-      var keySha1 = crypto.createHash('sha1').update(keyBase).digest('hex');
-      var keyHex = keySha1.slice(0, td.getKeySize());
-      return new Buffer(keyHex);
-    })();
-    td.open(key, token.iv);
+    td.open(keyBuf, token.iv);
     var cipherText = new Buffer(token.signature, 'base64');
     var decrypted = td.decrypt(cipherText).toString().replace(/\x00/g, '');
     assert(decrypted);
     var tokenObject = JSON.parse(decrypted);
+    assert.equal(tokenObject.app, 'brik-video-test');
+    assert(tokenObject.time);
+    assert(tokenObject.salt);
+  });
+
+  it("returns a valid token object (OpenSSL)", function() {
+    var token = app.getAuthenticationToken();
+    var ivBuf = new Buffer(token.iv);
+    var decipher = crypto.createDecipheriv('AES-256-CBC', keyBuf, ivBuf);
+    decipher.setAutoPadding(false);
+
+    var cipherText = new Buffer(token.signature, 'base64');
+    var plainText = decipher.update(token.signature, 'base64', 'utf8');
+    plainText += decipher.final('utf8');
+
+    assert(plainText);
+    var tokenObject = JSON.parse(plainText.replace(/\x00/g, ''));
     assert.equal(tokenObject.app, 'brik-video-test');
     assert(tokenObject.time);
     assert(tokenObject.salt);
